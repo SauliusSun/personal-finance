632691886e1da42e86fbc1b353904cbbd2bb42a4
Saulius Sunauskas <saulius.sunauskas@devbridge.com>
2019-11-20 17:28:06 +0200

    Add empty Budget API controller

---
+++ Personal Finance/BudgetController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Personal_Finance
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BudgetController : ControllerBase
+    {
+        [Route("")]
+        public ActionResult Get()
+        {
+            return Ok("Empty");
+        }
+    }
+}
